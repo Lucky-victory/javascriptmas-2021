be320bc0e22b74eca44eb28fefe1c73d95a448f4
lucky-victory <blizwonder25@gmail.com>
2021-12-09 23:17:02 +0100

    updated turn on decorations challenge

--- turn-on-the-decorations/main.js
+++ turn-on-the-decorations/main.js
@@ -1,7 +1,9 @@
 const house=document.querySelector('.house');
 const decoratorSwitch=document.querySelector('.decorator-switch');
+const decoratorLabel=document.querySelector('.decorator-label');
 decoratorSwitch.addEventListener('click',decorateHouse);
 
 function decorateHouse() {
   decoratorSwitch.checked ? house.classList.add('decorate') : house.classList.remove('decorate');
+  decoratorSwitch.checked ? decoratorLabel.textContent ='Turn off Decorations' : decoratorLabel.textContent ='Turn on Decorations'
 }
