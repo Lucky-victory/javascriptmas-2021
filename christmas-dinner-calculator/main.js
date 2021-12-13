dea4177ffaa27d334a1ef21de5cb7a773142a842
lucky-victory <blizwonder25@gmail.com>
2021-12-13 23:08:36 +0100

    updated main.js for christmas dinner calculator

--- christmas-dinner-calculator/main.js
+++ christmas-dinner-calculator/main.js
@@ -1,34 +1,73 @@
 const numOfGuestInput=document.querySelector('#num_of_guest_input');
 const guestForm=document.querySelector('.form');
-const dietaryCheckboxes=document.querySelectorAll('.radio');
 const suggestedDinnerElem=document.querySelector('.suggested-dinner');
 
 const dinnerSection=document.querySelector('.dinner-section');
-let hel=500
+let numOfGuest=0;
 
-const  christmasDinners=[{
-  image:'./images/turkey.jpg',
-  info:'To feed eight people, you will need a turkey weighing a minimum of 3.5kg to satisfy your dinner guests.'
-},
-{
-  image:'./images/roast-beef.jpg',
-  info:"Decided not to feed your guests turkey this year? If the joint is off the bone, allow 250g per serving – so 2kg for eight people."
-},
-{
-  image:'./images/pudding.jpg',
-info:`When it comes to Christmas pudding, we reckon ${hel}g pudding will be plenty to feed eight`
-}
-]
-console.log(christmasDinners[2].info);
 
 guestForm.addEventListener('submit',guestFormHandler);
 
 function guestFormHandler(evt) {
   evt.preventDefault();
-
-  guestForm.reset();
+  if(numOfGuestInput.value ==='') return;
+renderDinner();
+  numOfGuestInput.value='';
 }
 
 function renderDinner(){
-  
+const dietaryChoice=document.querySelector('.radio:checked').value;
+numOfGuest=Number(numOfGuestInput.value);
+const filteredDinner=christmasDinners().filter((christmasDinner)=>{
+  return christmasDinner.guestType === dietaryChoice;
+});
+const randomDinner=filteredDinner[Math.floor(Math.random() * filteredDinner.length)];
+
+dinnerSection.innerHTML='<h3 class="loader"><span class="spinner"></span>loading suggestion...</h3>';
+suggestedDinnerElem.textContent='...'
+setTimeout(()=>{
+suggestedDinnerElem.textContent=randomDinner.dinnerName;
+  dinnerSection.innerHTML='';
+  dinnerSection.insertAdjacentHTML('beforeend',` <div class="dinner-image-wrapper">
+        <img src="${randomDinner.image}" alt="${randomDinner.alt}" class="dinner-image" />
+      </div>
+      <p class="dinner-info">
+      ${randomDinner.info}
+     </p>`);
+},2000);
+     
+}
+
+function christmasDinners(){
+  return ([{
+        dinnerName: 'turkey',
+        image: './images/turkey.jpg',
+        alt:'turkey',
+        info: `To feed ${numOfGuest} people, you will need ${Math.floor((numOfGuest / 8)) > 1 ? Math.floor(numOfGuest / 8) : 'a'} ${Math.floor(numOfGuest  / 8) > 1 ? 'turkeys' :'turkey'} weighing a minimum of 3.5kg to satisfy your dinner guests.`,
+        guestType: 'meat-lover'
+    
+    },
+      {
+        dinnerName: 'Roast beef',
+        image: './images/roast-beef.jpg',
+        alt:'roast beef',
+        info: `Decided not to feed your guests turkey this year? If the joint is off the bone, allow 250g per serving – so ${(numOfGuest * 250)/1000}kg for ${numOfGuest} ${numOfGuest > 1 ? 'people' : 'person'}.`,
+        guestType: 'meat-lover'
+    
+    },
+      {
+        dinnerName: 'Christmas pudding',
+        image: './images/pudding.jpg',
+        alt:'Christmas pudding',
+        info: `When it comes to Christmas pudding, we reckon ${Math.floor(numOfGuest * 112.5)}g pudding will be plenty to feed ${numOfGuest} guests.`,
+        guestType: 'meat-lover'
+    },
+      {
+        dinnerName: 'spiced baklava',
+        image: './images/baklava.jpg',
+        alt:'spiced baklava',
+        info: `As a `,
+        guestType: 'vegetarian'
+    }
+    ])
 }
