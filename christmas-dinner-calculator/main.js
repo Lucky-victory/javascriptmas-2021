3eb543ed7c853655b3cb69ddf821fc70530d0ebd
lucky-victory <blizwonder25@gmail.com>
2021-12-13 23:44:27 +0100

    completed christmas dinner calculator

--- christmas-dinner-calculator/main.js
+++ christmas-dinner-calculator/main.js
@@ -29,7 +29,7 @@ setTimeout(()=>{
 suggestedDinnerElem.textContent=randomDinner.dinnerName;
   dinnerSection.innerHTML='';
   dinnerSection.insertAdjacentHTML('beforeend',` <div class="dinner-image-wrapper">
-        <img src="${randomDinner.image}" alt="${randomDinner.alt}" class="dinner-image" />
+        <img src="${randomDinner.image}" alt="${randomDinner.dinnerName}" class="dinner-image" />
       </div>
       <p class="dinner-info">
       ${randomDinner.info}
@@ -42,7 +42,7 @@ function christmasDinners(){
   return ([{
         dinnerName: 'turkey',
         image: './images/turkey.jpg',
-        alt:'turkey',
+    
         info: `To feed ${numOfGuest} people, you will need ${Math.floor((numOfGuest / 8)) > 1 ? Math.floor(numOfGuest / 8) : 'a'} ${Math.floor(numOfGuest  / 8) > 1 ? 'turkeys' :'turkey'} weighing a minimum of 3.5kg to satisfy your dinner guests.`,
         guestType: 'meat-lover'
     
@@ -50,7 +50,7 @@ function christmasDinners(){
       {
         dinnerName: 'Roast beef',
         image: './images/roast-beef.jpg',
-        alt:'roast beef',
+    
         info: `Decided not to feed your guests turkey this year? If the joint is off the bone, allow 250g per serving – so ${(numOfGuest * 250)/1000}kg for ${numOfGuest} ${numOfGuest > 1 ? 'people' : 'person'}.`,
         guestType: 'meat-lover'
     
@@ -58,15 +58,22 @@ function christmasDinners(){
       {
         dinnerName: 'Christmas pudding',
         image: './images/pudding.jpg',
-        alt:'Christmas pudding',
+    
         info: `When it comes to Christmas pudding, we reckon ${Math.floor(numOfGuest * 112.5)}g pudding will be plenty to feed ${numOfGuest} guests.`,
         guestType: 'meat-lover'
     },
       {
         dinnerName: 'spiced baklava',
         image: './images/baklava.jpg',
-        alt:'spiced baklava',
-        info: `As a `,
+      
+        info: ` A festive savoury spin on the classic Middle Eastern dessert makes a fantastic vegetarian substitute for turkey this Christmas, ${numOfGuest * 57}g should be enough for ${numOfGuest} guest.`,
+        guestType: 'vegetarian'
+    },
+      {
+        dinnerName: 'cauliflower wellington',
+        image: './images/cauliflower.webp',
+        info: `Cauliflower wellington
+This impressive veggie wellington swaps beef for saffron-spiced cauliflower, and it wouldn’t look out of place at any festive feast. you will need ${numOfGuest * 90}g for ${numOfGuest} guests.`,
         guestType: 'vegetarian'
     }
     ])
