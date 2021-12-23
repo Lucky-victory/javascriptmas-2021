fff411f62083040cb04fcee9c76086e67fe9999e
lucky-victory <blizwonder25@gmail.com>
2021-12-23 14:44:43 +0100

    started customisable santa sign

---
+++ customisable-santa-sign/main.js
@@ -0,0 +1,18 @@
+// Task: // - Write a function to update the Santa Stop Here sign with the user's own text. // Stretch goals // - Add a min and max length to the message. // - Add interchangeable emojis. // - Allow the user to customize the colors and other styling elements, too., 
+
+const inputElem=document.querySelector('.input');
+const errorMessage=document.querySelector('.error');
+inputElem.addEventListener('keyup',()=>{
+  renderSign()
+});
+function renderSign(){
+  
+}
+
+function changeEmoji(){
+  
+}
+
+function changeColor(){
+  
+}
